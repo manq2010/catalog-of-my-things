4f9119c19d96c1384f670013ff71211403f08f6c
Ibnballo1 <belloabdullateef035@gmail.com>
2023-02-14 20:00:57 +0100

    Add super to constructor

--- modules/music_album.rb
+++ modules/music_album.rb
@@ -1,11 +1,12 @@
 class MusicAlbum < Item
   attr_accessor :on_spotify
 
-  def initialize (on_spotify)
+  def initialize(on_spotify)
+    super()
     @on_spotify = on_spotify
   end
 
   private
-  def can_be_archived?
-  end
+
+  def can_be_archived?; end
 end

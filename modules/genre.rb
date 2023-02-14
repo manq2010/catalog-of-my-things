697a116636442e1f30db2c2cb630834c8d035c52
Ibnballo1 <belloabdullateef035@gmail.com>
2023-02-14 12:37:08 +0100

    Add genre class with methods

---
+++ modules/genre.rb
@@ -0,0 +1,13 @@
+class Genre < Item
+  attr_accessor :name
+
+  def initialize(name)
+    super()
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = []
+  end
+
+  def add_item
+  end
+end

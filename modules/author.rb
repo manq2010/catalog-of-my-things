0793218293907917b23888a6f61ec945147fc89a
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2023-02-16 01:15:20 +0200

    add author class

---
+++ modules/author.rb
@@ -0,0 +1,18 @@
+require_relative './item'
+
+class Author
+  attr_accessor :first_name, :last_name
+  attr_reader :id, :items
+
+  def initialize(first_name, last_name)
+    @id = Random.rand(1..10_000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item unless items.include?(self)
+    item.author = self
+  end
+end

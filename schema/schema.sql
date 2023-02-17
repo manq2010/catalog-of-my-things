627b444feb82bcb6cd90a887a47f31cc54ffd98d
manq2010 <manq2010@gmail.com>
2023-02-17 16:13:34 +0200

    Update schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE
-    Item (
+    item (
         id SERIAL PRIMARY KEY,
         genre VARCHAR(255) NOT NULL,
         author VARCHAR(255) NOT NULL,
@@ -8,36 +8,41 @@ CREATE TABLE
         archived BOOLEAN NOT NULL,
         label_id INT NOT NULL REFERENCES label (id),
         author_id INT NOT NULL REFERENCES author (id),
-        book_id INT NOT NULL REFERENCES book (id)
+        book_id INT NOT NULL REFERENCES book (id),
+        game_id INT NOT NULL REFERENCES game (id),
+        genre_id INT NOT NULL REFERENCES genre (id),
+        music_album_id INT NOT NULL REFERENCES music_album (id)
     );
 
-CREATE INDEX label_id_index ON Item (label_id);
+CREATE INDEX label_id_index ON item (label_id);
 
-CREATE INDEX book_id_index ON Item (book_id);
+CREATE INDEX book_id_index ON item (book_id);
 
-CREATE INDEX author_id_index ON Item (author_id);
+CREATE INDEX author_id_index ON item (author_id);
+
+CREATE INDEX game_id_index ON item (game_id);
+
+CREATE INDEX genre_id_index ON item (genre_id);
+
+CREATE INDEX music_album_id_index ON item (music_album_id);
 
 CREATE TABLE
-    Label (
+    label (
         id SERIAL PRIMARY KEY,
         title VARCHAR(255) NOT NULL,
         color VARCHAR(255) NOT NULL,
         items VARCHAR(255) CHECK (items IN ('')) NOT NULL
     );
 
-CREATE INDEX title_idx ON Label (title);
-
 CREATE TABLE
-    Book (
+    book (
         id INTEGER NOT NULL,
         publisher VARCHAR(255) NOT NULL,
         cover_state VARCHAR(255) NOT NULL
     );
 
-CREATE INDEX publisher_idx ON Book (publisher);
-
 CREATE TABLE
-    Author (
+    author (
         id SERIAL NOT NULL,
         first_name VARCHAR(50) NOT NULL,
         last_name VARCHAR(50) NOT NULL,
@@ -45,7 +50,7 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    Game (
+    game (
         id SERIAL NOT NULL,
         name VARCHAR(50) NOT NULL,
         multiplayer BOOLEAN NOT NULL,
@@ -54,16 +59,15 @@ CREATE TABLE
         PRIMARY KEY (id)
     );
 
-    
 CREATE TABLE
-    MusicAlbum (
+    music_album (
         id INTEGER NOT NULL,
         on_spotify VARCHAR(150) NOT NULL,
         PRIMARY KEY (id)
     );
 
 CREATE TABLE
-    Genre (
+    genre (
         id SERIAL NOT NULL,
         name VARCHAR(50) NOT NULL,
         publish_date DATE NOT NULL,

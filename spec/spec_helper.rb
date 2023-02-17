44e81fd087040aaca52c5745bfc2782a25027abc
Ibnballo1 <belloabdullateef035@gmail.com>
2023-02-17 10:24:01 +0100

    Add tests for music album

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require_relative '../modules/book'
 require_relative '../modules/label'
+require_relative '../modules/music_album'
+require_relative '../modules/genre'
 require_relative '../user_interface/book_ui'
+require_relative '../user_interface/music_album_ui'
 
 require 'json'
